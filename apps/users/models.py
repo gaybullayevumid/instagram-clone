1a1fbf9770d6e3c64eeb5c18391c8743bb952520
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-10-21 15:31:10 +0500

    Implement user model enhancements: add username, email, and password validation; integrate JWT token generation; update settings and requirements for JWT support

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,6 +4,8 @@ from apps.shared.models import BaseModel
 from datetime import datetime, timedelta
 from django.core.validators import FileExtensionValidator
 import random
+import uuid
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your models here.
 
@@ -52,6 +54,42 @@ class User(AbstractUser, BaseModel):
             user_id=self.id, verify_type=verify_type, code=code
         )
         return code
+
+    def check_username(self):
+        if not self.username:
+            temp_username = f"instagram-{uuid.uuid4().__str__().split("-")[-1]}"
+            while User.objects.filter(username=temp_username):
+                temp_username = f"{temp_username}{random.randint(0, 9)}"
+            self.username = temp_username
+
+    def check_email(self):
+        if self.email:
+            normalize_email = self.email.lower()
+            self.email = normalize_email
+
+    def check_pass(self):
+        if not self.password:
+            temp_password = f"password-{uuid.uuid4().__str__().split("-")[-1]}"
+            self.password = temp_password
+
+    def hashing_password(self):
+        if not self.password.startswith("pbkdf2_sha256"):
+            self.set_password(self.password)
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {"access": str(refresh.access_token), "refresh": str(refresh)}
+
+    def clean(self):
+        self.check_email()
+        self.check_username(),
+        self.check_pass(),
+        self.hashing_password()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.clean()
+        super(User, self).save(*args, **kwargs)
 
 
 PHONE_EXPIRE = 2
